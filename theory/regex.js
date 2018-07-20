568bcb824140010def1f4fc618c767d0f11605a1
JustinSHong <jhong2009@gmail.com>
2018-07-20 10:32:40 -0700

    in regex.js: set up regex to match goat and moat

--- theory/regex.js
+++ theory/regex.js
@@ -13,7 +13,10 @@ console.log(`regex matches antelopes: ${regex1.test('antelopes')}`);
 // moat
 // but not:
 // boat
-
+const regex2 = /([g|m]oat)/;
+console.log(`regex matches goat: ${regex2.test('goat')}`);
+console.log(`regex matches moat: ${regex2.test('moat')}`);
+console.log(`regex matches boat: ${regex2.test('boat')}`);
 // Regex that matches dates in YYYY-MM-DD format. (Year can be 1-4 digits, and month and day can each be 1-2 digits). 
 // This does not need to verify the date is correct (e.g 3333-33-33 can match).
 // 2000-10-12
