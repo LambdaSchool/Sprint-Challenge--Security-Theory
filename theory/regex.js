92067951dab2b5a436b7f06e6032ffe199b04714
JustinSHong <jhong2009@gmail.com>
2018-07-20 10:35:42 -0700

    in regex.js set up regex to match YYYY-MM-DD date format

--- theory/regex.js
+++ theory/regex.js
@@ -19,6 +19,11 @@ console.log(`regex matches moat: ${regex2.test('moat')}`);
 console.log(`regex matches boat: ${regex2.test('boat')}`);
 // Regex that matches dates in YYYY-MM-DD format. (Year can be 1-4 digits, and month and day can each be 1-2 digits). 
 // This does not need to verify the date is correct (e.g 3333-33-33 can match).
+const regex3 = /(\d){1,4}-(\d){1,2}-(\d){1,2}/;
+console.log(`regex matches 2000-10-12: ${regex3.test('2000-10-12')}`);
+console.log(`regex matches 1999-1-20: ${regex3.test('1999-1-20')}`);
+console.log(`regex matches 1999-01-20: ${regex3.test('1999-01-20')}`);
+console.log(`regex matches 812-2-10: ${regex3.test('812-2-10')}`);
 // 2000-10-12
 // 1999-1-20
 // 1999-01-20
