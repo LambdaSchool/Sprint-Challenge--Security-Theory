1269085927fd805994d7cccfb60054aa21202f49
Anne Courtney <annecourtney_1@yahoo.com>
2018-08-03 11:36:07 -0500

    completed regexes and state machine diagrams - sprint complete

---
+++ theory/regex.js
@@ -0,0 +1,13 @@
+
+
+
+// antelope/antelopes regex
+const regex = /(\w+s?){8,9}/g;
+
+// goat/moat/not boat regex
+const regex = /(?!boat)(\w+){4}/g;
+
+// dates regex
+const regex = /(\d+){3,4}\-(\d+){1,2}\-(\d+){2}/g;
+
+
