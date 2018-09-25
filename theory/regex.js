295ef4ffe741176ccf899a636b6f0fe57f2f3756
Victor Montoya <victoramontoya@gmail.com>
2018-09-24 19:07:17 -0700

    cloned correct repo

---
+++ theory/regex.js
@@ -0,0 +1,11 @@
+1, 4
+
+var rocks = /antelopes?rocks?out/g
+
+var boat = /[gm]oat?rocks?out/g
+
+var both= /(g | m)(oat)/g
+
+var a = /\{1,4}/g
+
+var a = /\{1,4}\-\d{1,2}\-\d{1,2}/g
---
+++ theory/timeComplexity.md
@@ -0,0 +1,8 @@
+Exercise I
+
+a) O(n)
+b) O (log n)
+c) O(sqrt(n))
+d) On(log n) 
+e) O(n²) 
+f)
